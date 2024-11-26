cdaa0701fbe5cc64d1bfb76f35ae7af71d5a53a3
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-11-26 21:52:15 +0600

    store create

---
+++ src/app/Store.jsx
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { counterReducer } from "./featured/counter/CounterSlice";
+
+const store=configureStore({
+    reducer:{
+        counter:counterReducer,
+    },
+})
+export default store

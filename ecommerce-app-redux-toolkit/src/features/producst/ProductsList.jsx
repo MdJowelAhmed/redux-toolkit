9628fa5bcf6a145a7bb8829f5128d23a6364d030
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-11-29 08:43:35 +0600

    data load by async thunk

--- ecommerce-app-redux-toolkit/src/features/producst/ProductsList.jsx
+++ ecommerce-app-redux-toolkit/src/features/producst/ProductsList.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { fetchProducts } from './productSlice'
 
 const ProductsList = () => {
+const {products, isLoading, error}=useSelector((state)=>state.productsR)
+console.log(products)
   const dispatch=useDispatch()
   useEffect(()=>{
     dispatch(fetchProducts())
-  })
+  },[dispatch])
 
   return (
     <div>ProductsList</div>
--- ecommerce-app-redux-toolkit/src/features/producst/productSlice.js
+++ ecommerce-app-redux-toolkit/src/features/producst/productSlice.js
@@ -9,15 +9,27 @@ const initialState = {
 
 export const fetchProducts=createAsyncThunk("products/fetchProducts",async ()=>{
   const res=await axios.get("http://localhost:3001/products");
-  console.log(res.data)
+  return (res.data)
 })
 
 export const productSlice = createSlice({
   name: 'products',
   initialState,
-  reducers: {
-    
-  },
+  reducers: {  },
+  extraReducers: (builder) =>{
+    builder
+    .addCase(fetchProducts.pending, (state)=>{
+      state.isLoading=true;
+    })
+    .addCase(fetchProducts.fulfilled, (state,action)=>{
+      state.isLoading=false;
+      state.products=action.payload
+    })
+    .addCase(fetchProducts.rejected, (state,action)=>{
+      state.isLoading=false;
+      state.error=action.error.message || "data fetch field"
+    })
+  }
 })
 
 

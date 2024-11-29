a7606310a7ac49264e948439e0c5ee1b0e707258
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-11-29 08:26:08 +0600

    json server by api call

--- ecommerce-app-redux-toolkit/src/features/producst/ProductsList.jsx
+++ ecommerce-app-redux-toolkit/src/features/producst/ProductsList.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { fetchProducts } from './productSlice'
 
 const ProductsList = () => {
+  const dispatch=useDispatch()
+  useEffect(()=>{
+    dispatch(fetchProducts())
+  })
+
   return (
     <div>ProductsList</div>
   )
--- ecommerce-app-redux-toolkit/src/features/producst/productSlice.js
+++ ecommerce-app-redux-toolkit/src/features/producst/productSlice.js
@@ -1,10 +1,16 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
 
 const initialState = {
  products:[],
  isLoading: false,
  error: null,
 }
+
+export const fetchProducts=createAsyncThunk("products/fetchProducts",async ()=>{
+  const res=await axios.get("http://localhost:3001/products");
+  console.log(res.data)
+})
 
 export const productSlice = createSlice({
   name: 'products',

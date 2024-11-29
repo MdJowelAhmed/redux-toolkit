1d88e5f693132afa0f5a5fcd5dd751aea2079d59
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-11-29 09:29:33 +0600

    successfully data fetch

--- ecommerce-app-redux-toolkit/src/features/producst/ProductsList.jsx
+++ ecommerce-app-redux-toolkit/src/features/producst/ProductsList.jsx
@@ -1,18 +1,41 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchProducts } from './productSlice'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "./productSlice";
 
 const ProductsList = () => {
-const {products, isLoading, error}=useSelector((state)=>state.productsR)
-console.log(products)
-  const dispatch=useDispatch()
-  useEffect(()=>{
-    dispatch(fetchProducts())
-  },[dispatch])
+  const { products, isLoading, error } = useSelector(
+    (state) => state.productsR
+  );
+  console.log(products);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
 
   return (
-    <div>ProductsList</div>
-  )
-}
+    <div className="container mx-auto">
+      {isLoading && <p> Loading......</p>}
+      {error && <p> {error}</p>}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        {products && products.length > 0 ? (
+          products.map((product) => {
+            return (
+              <article key={product.id}>
+                <div className="p-10 shadow-lg rounded-lg h-48">
+                  <h2>{product.title}</h2>
+                  <p>{product.description} </p>
+                  <p>{product.price} </p>
+                  <p>{product.category} </p>
+                </div>
+              </article>
+            );
+          })
+        ) : (
+          <p>No data available</p>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default ProductsList
+export default ProductsList;
